1c8543a6859459fb8cc88991ddcd204f976be8d1
Minato Feng <feng1m8@outlook.com>
2025-01-14 15:40:41 +0800

    Add initialize.cxx

--- src/offaxis/reflection/spectrum.cxx
+++ src/offaxis/reflection/spectrum.cxx
@@ -1,14 +1,9 @@
 #include <functional>
 #include <memory>
 
-#include <stdlib.h>
-
 #include "offaxis/convolve/convolve.hxx"
-#include "offaxis/envs.hxx"
 #include "offaxis/parameter.hxx"
 #include "spectrum.hxx"
-
-extern "C" int version_number_printed;
 
 namespace offaxis::relxill
 {
@@ -71,62 +66,4 @@ namespace offaxis::relxill
 
         return tab->n_incl;
     }
-
-    char *get_full_path_table_name(const char *filename, int *status)
-    {
-        auto env = std::getenv("OFFAXIS_TABLE_PATH");
-        if (env != nullptr)
-        {
-            auto fp = std::filesystem::path(env) / filename;
-            if (std::filesystem::exists(fp))
-            {
-                char *fullfilename = new char[1 + fp.string().size()]{'\0'};
-                fp.string().copy(fullfilename, fp.string().size());
-                return fullfilename;
-            }
-            else
-                throw std::system_error(std::make_error_code(std::errc::no_such_file_or_directory), fp.string());
-        }
-
-        env = std::getenv("RELXILL_TABLE_PATH");
-        if (env != nullptr)
-        {
-            auto fp = std::filesystem::path(env) / filename;
-            if (std::filesystem::exists(fp))
-            {
-                char *fullfilename = new char[1 + fp.string().size()]{'\0'};
-                fp.string().copy(fullfilename, fp.string().size());
-                return fullfilename;
-            }
-            else
-                throw std::system_error(std::make_error_code(std::errc::no_such_file_or_directory), fp.string());
-        }
-
-        auto fp = std::filesystem::current_path() / filename;
-        if (std::filesystem::exists(fp))
-        {
-            char *fullfilename = new char[1 + fp.string().size()]{'\0'};
-            fp.string().copy(fullfilename, fp.string().size());
-            return fullfilename;
-        }
-
-        fp = utils::abspath().replace_filename(filename);
-        if (std::filesystem::exists(fp))
-        {
-            char *fullfilename = new char[1 + fp.string().size()]{'\0'};
-            fp.string().copy(fullfilename, fp.string().size());
-            return fullfilename;
-        }
-        else
-            throw std::system_error(std::make_error_code(std::errc::no_such_file_or_directory), fp.string());
-
-        return nullptr;
-    }
-
-    static const int init = []()
-    {
-        getFullPathTableName = get_full_path_table_name;
-        version_number_printed = 1;
-        return 0;
-    }();
 }
